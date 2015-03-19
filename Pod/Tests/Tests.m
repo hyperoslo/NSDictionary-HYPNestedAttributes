6632a0a63e1b2c3d58346a433d0de9174c585e6c
Elvis Nuñez <elvisnunez@me.com>
2015-03-19 11:02:33 +0100

    Add failing test

--- Pod/Tests/Tests.m
+++ Pod/Tests/Tests.m
@@ -9,7 +9,7 @@
 
 @implementation PodTests
 
-- (void)testNestify
+- (void)testNestifyA
 {
     NSDictionary *dictionary = @{@"first_name" : @"Chris",
                                  @"contacts[0].name" : @"Tim",
@@ -25,6 +25,21 @@
 
     NSDictionary *resultDictionary = @{@"first_name" : @"Chris",
                                        @"contacts_attributes" : @[contactFirst, contactSecond]};
+
+    XCTAssertEqualObjects([dictionary hyp_nestify], resultDictionary);
+}
+
+- (void)testNestifyB
+{
+    NSDictionary *dictionary = @{@"contacts[0].id" : @0,
+                                 @"notes[0].id" : @0};
+
+    NSDictionary *contact = @{@"0" : @{@"id" : @0}};
+
+    NSDictionary *note = @{@"0" : @{@"id" : @0}};
+
+    NSDictionary *resultDictionary = @{@"contacts_attributes" : @[contact],
+                                       @"notes_attributes" : @[note]};
 
     XCTAssertEqualObjects([dictionary hyp_nestify], resultDictionary);
 }
