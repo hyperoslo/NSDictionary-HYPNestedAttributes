bbfff0b02683f1080f5088e905a8c07c1181b973
Elvis Nuñez <elvisnunez@me.com>
2015-03-20 15:01:03 +0100

    Add tests

--- Pod/Tests/Tests.m
+++ Pod/Tests/Tests.m
@@ -94,11 +94,32 @@
                                  @"notes[0].id" : @0};
 
     NSDictionary *contact = @{@"0" : @{@"id" : @0}};
-
     NSDictionary *note = @{@"0" : @{@"id" : @0}};
+    NSDictionary *resultDictionary = @{@"contacts_attributes" : contact,
+                                       @"notes_attributes" : note};
 
-    NSDictionary *resultDictionary = @{@"contacts_attributes" : @[contact],
-                                       @"notes_attributes" : @[note]};
+    XCTAssertEqualObjects([dictionary hyp_railsNestedAttributes], resultDictionary);
+}
+
+- (void)testRailsNestedAttributesC
+{
+    NSDictionary *dictionary = @{@"children[0].birthdate" : @"2014-03-20 13:15:26 +0000",
+                                 @"children[0].name" : @"Hallo",
+                                 @"contacts[0].name" : @"Hei there",
+                                 @"contacts[0].phone_number" : @"7312312",
+                                 @"contacts[1].name" : @"Parorent",
+                                 @"contacts[1].phone_number" : @"23124421"};
+
+    NSDictionary *children = @{@"0" : @{@"birthdate" : @"2014-03-20 13:15:26 +0000",
+                                        @"name" : @"Hallo"}};
+
+    NSDictionary *contacts = @{@"0" : @{@"name" : @"Hei there",
+                                        @"phone_number" : @"7312312"},
+                               @"1" : @{@"name" : @"Parorent",
+                                        @"phone_number" : @"23124421"}};
+
+    NSDictionary *resultDictionary = @{@"children_attributes" : children,
+                                       @"contacts_attributes" : contacts};
 
     XCTAssertEqualObjects([dictionary hyp_railsNestedAttributes], resultDictionary);
 }
